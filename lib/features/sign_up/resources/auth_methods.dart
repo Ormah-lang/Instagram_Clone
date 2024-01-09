b66b474e02f9fa7fc87bf2c276f38cd422935902
Chioma Onyeneke <ccchioma60@gmail.com>
2024-01-09 17:04:01 +0000

    upload image to firebase storage

--- lib/features/sign_up/resources/auth_methods.dart
+++ lib/features/sign_up/resources/auth_methods.dart
@@ -2,6 +2,8 @@ import 'dart:typed_data';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/features/sign_up/resources/storage_methods.dart';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -22,18 +24,24 @@ class AuthMethods {
         UserCredential userCredential = await _auth
             .createUserWithEmailAndPassword(email: email, password: password);
 
+        String photoUrl = await StorageMethods()
+            .uploadImageToStorage('profilePics', file, false);
+
         _firestore.collection('users').doc(userCredential.user!.uid).set({
           'username': username,
           'uid': userCredential.user!.uid,
           'email': email,
           'bio': bio,
+          'photoUrl': photoUrl,
           'followers': [],
           'following': []
         });
         res = 'success';
+        debugPrint('res: $res');
       }
     } catch (err) {
       res = err.toString();
+      debugPrint('res: $res');
     }
     return res;
   }
---
+++ lib/features/sign_up/resources/storage_methods.dart
@@ -0,0 +1,21 @@
+import 'dart:typed_data';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class StorageMethods {
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<String> uploadImageToStorage(
+      String childName, Uint8List file, bool isPost) async {
+    UploadTask uploadTask = _storage
+        .ref()
+        .child(childName)
+        .child(_auth.currentUser!.uid)
+        .putData(file);
+
+    TaskSnapshot snapshot = await uploadTask;
+    return await snapshot.ref.getDownloadURL();
+  }
+}
